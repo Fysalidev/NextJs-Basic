df7632a9b7ecc00ba26ffdc2bd153b5a4e817016
ROBILLIARD Grégory <gregory.robilliard@icloud.com>
2023-03-10 10:49:23 +0100

    End of work

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,11 +1,28 @@
-import styles from '@/styles/Ninjas.module.css'
+import styles from "@/styles/Ninjas.module.css";
+import Link from "next/link";
 
-const Ninjas = () => {
-    return ( 
-        <div>
-            <h1>All Ninjas</h1>
-        </div>
-     );
-}
- 
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data },
+  };
+};
+
+const Ninjas = ({ ninjas }) => {
+  return (
+    <div>
+      <h1>All Ninjas</h1>
+      {ninjas.map((ninja) => (
+        <Link href={"/ninjas/" + ninja.id} key={ninja.id}>
+          <span className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </span>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
 export default Ninjas;
